f3d64bad5edfe49b833155ea4ddad171ab2a0ed7
nyomo <nyomoko@gmail.com>
2019-07-03 18:59:42 +0900

    versiosではなくてversion

--- lib/slack_goodies.rb
+++ lib/slack_goodies.rb
@@ -1,5 +1,5 @@
 module SlackGoodies
-  require_relative "slack_goodies/versions"
+  require_relative "slack_goodies/version"
   require_relative "slack_goodies/connection"
   require_relative "slack_goodies/cli"
 end
