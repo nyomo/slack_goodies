a0536f95dd83203dddb9a18830a8eb6ce357dab0
nyomo <nyomoko@gmail.com>
2019-11-04 23:46:56 +0900

    doubleをやめてvcrにした

--- spec/slack_goodies_spec.rb
+++ spec/slack_goodies_spec.rb
@@ -7,11 +7,10 @@ VCR.configure do |config|
 end
 describe SlackGoodies::Connection do
    it "public channelの一覧が取得出来る" do
-      slack_api_mock = double("Slack API")
-      allow(slack_api_mock).to receive(:channels_list)
       connection = SlackGoodies::Connection.new
-      allow(connection).to receive(:slack_client).and_return(slack_api_mock)
-      expect { connection.channel_list }.not_to raise_error
+      VCR.use_cassette("channels_list") do
+        expect { connection.channel_list }.not_to raise_error
+      end
    end
 end
 
